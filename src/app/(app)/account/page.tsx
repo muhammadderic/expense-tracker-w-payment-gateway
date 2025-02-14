db4e948a74e4769c13c6ea18da9486652c39ab10
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-14 07:13:52 +0700

    Update 'account' page to show email or given name

--- src/app/(app)/account/page.tsx
+++ src/app/(app)/account/page.tsx
@@ -1,7 +1,28 @@
-const Page = () => {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <div>
-      <h1>Account page</h1>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-white">Account</h1>
+
+        {
+          user.email ? (
+            <p className="text-white mt-2">
+              Logged in with email: <span className="font-bold">{user.email}</span>
+            </p>
+          ) : (
+            <p className="text-white mt-2">
+
+              Logged in with given name: <span className="font-bold">{user.given_name}</span>
+            </p>
+          )
+        }
+
+      </div>
     </div>
   )
 }
